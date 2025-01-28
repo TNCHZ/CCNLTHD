bc25c5ce16ebb398e2e4efaecac64cb15db3180e
luhieutrung2602@gmail.com <luhieutrung2602@gmail.com>
2025-01-28 20:26:35 +0700

    Trung

--- quanlychungcu/aparment/aparmentapis/settings.py
+++ quanlychungcu/aparment/aparmentapis/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(_file_).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 from django.conf.global_settings import AUTH_USER_MODEL
 AUTH_USER_MODEL = 'apartment.User'
